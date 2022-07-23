4b8f8d79fb6199159d9b4ddd1e61da0abcde6cb1
dangkl123 <nhatdang2604@gmail.com>
2022-07-23 11:02:31 +0700

    Add method to parse a page from a given title

--- wiki.go
+++ wiki.go
@@ -5,15 +5,36 @@ import (
 	"os"
 )
 
+const (
+	TEXT_FILE_EXTENSION = ".txt"
+)
+
 type page struct {
 	Title string
 	Body []byte
 }
 
+
+
 //Save a page body to text file
 func (p *page) save() error {
-	const filename = p.Title + ".txt"
+	const filename = p.Title + TEXT_FILE_EXTENSION
 	return os.WriteFile(filename, p.Body, 0600)
+}
+
+//Parse a page after reading it
+func loadPage(title string) (*Page, error) {
+	const filename = p.Title + TEXT_FILE_EXTENSION
+	body, err := os.ReadFile(filename)
+
+	//return the error if encoutering it
+	if nil != err {
+		return nil, err
+	}
+
+	//return the page with the nil error
+	return &Page{Title: title, Body: body}, err
+
 }
 
 
