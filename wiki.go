574e19a3cd7344af9f2ce3677f35413fdeba5009
dangkl123 <nhatdang2604@gmail.com>
2022-07-24 13:50:30 +0700

    change all data .txt file path into data/

--- wiki.go
+++ wiki.go
@@ -46,13 +46,15 @@ type page struct {
 //Save a page body to text file
 func (p *page) save() error {
 	filename := p.Title + TEXT_FILE_EXTENSION
-	return os.WriteFile(filename, p.Body, 0600)
+	filepath := DATA_FOLDER_PATH + filename
+	return os.WriteFile(filepath, p.Body, 0600)
 }
 
 //Parse a page after reading it
 func loadPage(title string) (*page, error) { 
 	filename := title + TEXT_FILE_EXTENSION
-	body, err := os.ReadFile(filename)
+	filepath := DATA_FOLDER_PATH + filename
+	body, err := os.ReadFile(filepath)
 
 	//return the error if encoutering it
 	if nil != err {
